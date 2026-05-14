e1e382baaa8453a7fabe73ea69b2a29441fddca7
Dharana Koirala <koiraladharana21@gmail.com>
2026-05-14 08:10:21 +0000

    feat: initial project setup with vite + react + typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './index.css'
+import heroImg from "./assets/hero.png"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>This is app page.</h1>
+        <img src= {heroImg} alt="hero" />
+        </div>
+    </>
+  )
+}
+
+export default App
